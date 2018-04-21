01522f54510e65390c23352f48419865e4b93ccf
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-04-21 10:22:44 -0400

    wip updated liri.js

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 
 var Twitter = require ('twitter');
-//var env = require('dotenv');
+//var env = require('dotenv'); It is already required in keys.js
 var Spotify = require ('node-spotify-api');
 var request = require ('request');
 var omdb = require ('omdb');
@@ -56,74 +56,75 @@ function doTweetFeed() {
           console.log("Error Status Code = " + response.statusCode);
       }
   });
-};
+}
 
 function spotifyThisSong(userInput) {
   //User will select a song, if they do not a default song will be chosen for them.
   if (userInput === undefined) {
-     song = "I Saw The Sign, Ace Of Base";
+    userInput = "I Saw The Sign, Ace Of Base";
 
   } else {  
-      if (userChoice === "Escapade by Janet Jackson")
-         //userChoice = "Sweet Caroline by Neil Diamond"
-         console.log(userInput);
+      if (userChoice === "Escapade by Janet Jackson"){
+         //userChoice === "Sweet Caroline by Neil Diamond" I used this for a test and it worked
+         console.log("*********", userInput);
+      }
   }
   spotify.search({ type: "track", query: userChoice}, function(err, data) {
     if (err) {
       console.log('err', err);
     } else {
-      //for (var i = 0; i < data.tracks.items.length; i++) {
-      console.log(data.tracks.items[0]);
-      console.log("spotify-this-song: " + JSON.parse(data.tracks.items[i].text, null, 2));
+      for (var i = 0; i < 1; i++) {
+      // console.log(data.tracks.items[0]);
+      // console.log("spotify-this-song: " + data.tracks.items[i].text);
       console.log('-----');
-      console.log('data', data);
+      // console.log('data', data.tracks.items[0]);
       console.log("Artist: " + data.tracks.items[0].artists[0].name);
        console.log("Song Name: " + data.tracks.items[0].name);
        console.log("Link: " + data.tracks.items[0].external_urls.spotify);
        console.log("Album Name: " + data.tracks.items[0].album.name);
       
-      }
-    //}
+     }
+    }
   });
-};
+}
 
 
 function doWhatItSays() {
-fs.readFile('./random.txt', 'utf8', function(err, data) {
-  if (err) {
-    console.log(err);
-  } else
-  console.log('data', data);
-  //console.log(data.toString());
-});
-
+  fs.readFile('./random.txt', 'utf8', function(err, data) {
+    if (err) {
+      console.log(err);
+    } else
+    console.log('data', data);
+    //console.log(data.toString());
+  });
+}
 		
 
-//function movieGrab (userChoice) { 
+function movieGrab (userChoice) { 
 
-  //if (userChoice === undefined) {
-    //   movieTitle = "Mr Nobody"; 
-  //}
+  if (userChoice === undefined) 
+      var movieTitle = "Mr Nobody"; 
+
+//This is the omdb API key with a user choice if the user puts one in to query
 //queryUrl =  "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=" + keys.omdb.key;
 
-//request ("http://www.omdbapi.com/?t=" + (userChoice || 'Mr Nobody') + "&y=&plot=short&apikey=" , function (error, response, body) { 
+request ("http://www.omdbapi.com/?t=" + (userChoice || 'Mr Nobody') + "&y=&plot=short&apikey=" , function (error, response, body) { 
 
-  // if (!error && response.statusCode === 200) {
-    //   console.log('');
-      // console.log ('Title' + JSON.parse(body).Title);
-       //console.log('Year: '+ JSON.parse(body).year);
-       //console.log('IMDB Rating: '+ JSON.parse(body).imdRbating);
-       //console.log('Rotten Tomatoes Rating:' + JSON.parse(body).tomatoRating);
-       //console.log('Rotten Tomatoes URL: ' + JSON.parse(body).tomatoURL);
-       //console.log ('Country: ' + JSON.parse(body).Country);
-       //console.log('Language: ' + JSON.parse(body).Language);
-       //console.log('Plot: ' + JSON.parse(body).Plot);
-       //console.log('Actors:' + JSON.parse(body).Actors);
-       //console.log('');
-
-
-  // } else {
-    //console.log(err);
-   //} 
-   //}
-//});
+   if (!error && response.statusCode === 200) {
+      console.log('');
+      console.log ('Title' + JSON.parse(body).Title);
+      console.log('Year: '+ JSON.parse(body).year);
+      console.log('IMDB Rating: '+ JSON.parse(body).imdRbating);
+      console.log('Rotten Tomatoes Rating:' + JSON.parse(body).tomatoRating);
+      console.log('Rotten Tomatoes URL: ' + JSON.parse(body).tomatoURL);
+      console.log ('Country: ' + JSON.parse(body).Country);
+      console.log('Language: ' + JSON.parse(body).Language);
+      console.log('Plot: ' + JSON.parse(body).Plot);
+      console.log('Actors:' + JSON.parse(body).Actors);
+      console.log('');
+ 
+    } else {
+    console.log(err);
+   } 
+  });
+  }
