505635414a3a4972572b2b696de5046dfda2d02d
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-04-21 01:17:31 -0400

    updated liri.js functions

--- keys.js
+++ keys.js
@@ -3,33 +3,17 @@ require('dotenv').config();
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: 'hIeEyy7CljkyLWGETqS1ESr33',
-  consumer_secret: 'rdm1MHJ50Hp9GYeCHrNyy8ZbSSqjaXJoiwvgKsBnaqK1tYOsq3',
-  access_token_key: '987126847006826496-ad4GdynGLwE6ft6fMeVd5jLKfpo4j78',
-  access_token_secret: 'p9O0zX7TMB8tW2yshthy1k6Xu8sbejQqFd4jf2HbNUCTa',
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: "74009f0a95af4f85bd8c944d48323ef6",
-  secret: "d52e025d59eb429bbd7850771f4110bc"
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 
-module.exports.twitterKeys = twitterKeys;
-module.exports.spotifyKeys = spotifyKeys;
-
-//This variable holds the omdb api key
-var omdbkey = "50483e6b";
-
-//This variable holds the twitter key info
-
-var twitterKeys = {
-  consumer_key: 'hIeEyy7CljkyLWGETqS1ESr33',
-  consumer_secret: 'rdm1MHJ50Hp9GYeCHrNyy8ZbSSqjaXJoiwvgKsBnaqK1tYOsq3',
-  access_token_key: '987126847006826496-ad4GdynGLwE6ft6fMeVd5jLKfpo4j78',
-  access_token_secret: 'p9O0zX7TMB8tW2yshthy1k6Xu8sbejQqFd4jf2HbNUCTa',
-};
-
-var spoitfyKeys = {
-  id: "74009f0a95af4f85bd8c944d48323ef6",
-  secret: "d52e025d59eb429bbd7850771f4110bc"
+exports.omdb = {
+  key: process.env.OMDB_KEY
 };
--- liri.js
+++ liri.js
@@ -1,18 +1,21 @@
 
-
 var Twitter = require ('twitter');
 //var env = require('dotenv');
 var Spotify = require ('node-spotify-api');
 var request = require ('request');
+var omdb = require ('omdb');
 
 var fs = require ('fs');
 var keys = require ('./keys.js');
 var commandList = process.argv[2];
 var userChoice = process.argv[3];
 var parameter = {screen_name: 'JPCoder777'};
+var spotifyParam = {user_name: 'jshaun300'};
+
 
 //var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 
 
@@ -24,7 +27,7 @@ switch (commandList) {
   spotifyThisSong(userChoice);
   break;
   case 'movie-this': 
-  movieThis(userChoice);
+  movieGrab(userChoice);
   break;
   case 'do-what-it-says':
   doWhatItSays();
@@ -32,16 +35,20 @@ switch (commandList) {
 }
 
 
-
 function doTweetFeed() {
 
   //Call to Twitter API to get timeline information
   client.get('statuses/user_timeline', { screen_name: 'JPCoder777' }, function(error, tweets, response) {
       if (!error) {
-          for (var i = 0; i <= 3; i++) {
-              console.log("Me Tweets: " + JSON.stringify(tweets[i].text, null, 2));
-              //console.log(tweets[i].created_at);
+          for (var i = 0; i < tweets.length; i++) {
+              var tweetsText = (tweets[i].text);
+              var tweetsCreated = (tweets[i].created_at);
+              console.log("my-tweets: " + JSON.parse(tweets[i].text, null, 2));
+              console.log(tweets[i].created_at);
+              console.log("Tweet:" + tweets[i].text);
+              //console.log ('Time' + tweets[i].created_at);
               console.log("It worked!");
+              //console.log(tweets[i].text);
           }
       } else {
           //error message to display if error found
@@ -51,82 +58,72 @@ function doTweetFeed() {
   });
 };
 
-function spotifyThisSong(commandList, userChoice) {
-
+function spotifyThisSong(userInput) {
   //User will select a song, if they do not a default song will be chosen for them.
-  if (commandList === "spotify-this-song") {
-     var song = userChoice;
-   if (song === undefined) {
-    //userChoice = "Jungle by Tash Sultanta";
-    console.log("Do you want Jungle by Tash Sultana? " + userChoice);
-    }    
-  }  
-  else {
-  var songData = response.tracks.items[0]
+  if (userInput === undefined) {
+     song = "I Saw The Sign, Ace Of Base";
+
+  } else {  
+      if (userChoice === "Escapade by Janet Jackson")
+         //userChoice = "Sweet Caroline by Neil Diamond"
+         console.log(userInput);
+  }
   spotify.search({ type: "track", query: userChoice}, function(err, data) {
-      // console.log(data.tracks.items[0]);
+    if (err) {
+      console.log('err', err);
+    } else {
+      //for (var i = 0; i < data.tracks.items.length; i++) {
+      console.log(data.tracks.items[0]);
+      console.log("spotify-this-song: " + JSON.parse(data.tracks.items[i].text, null, 2));
+      console.log('-----');
+      console.log('data', data);
       console.log("Artist: " + data.tracks.items[0].artists[0].name);
-      console.log("Song Name: " + data.tracks.items[0].name);
-      console.log("Link: " + data.tracks.items[0].external_urls.spotify);
-      console.log("Album Name: " + data.tracks.items[0].album.name);
-      console.log("-------------------");
+       console.log("Song Name: " + data.tracks.items[0].name);
+       console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+       console.log("Album Name: " + data.tracks.items[0].album.name);
+      
+      }
+    //}
   });
- };
 };
 
+
+function doWhatItSays() {
 fs.readFile('./random.txt', 'utf8', function(err, data) {
   if (err) {
     console.log(err);
-  }
-  //data = question;
+  } else
   console.log('data', data);
-  //song(data);
-})
+  //console.log(data.toString());
+});
 
+		
 
+//function movieGrab (userChoice) { 
 
-function movieThis(userChoice) {
+  //if (userChoice === undefined) {
+    //   movieTitle = "Mr Nobody"; 
+  //}
+//queryUrl =  "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=" + keys.omdb.key;
 
-  //var title = 'The Wolf of Wall Street'
+//request ("http://www.omdbapi.com/?t=" + (userChoice || 'Mr Nobody') + "&y=&plot=short&apikey=" , function (error, response, body) { 
 
-  queryUrl =  "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=" + omdbKey;
-
-  request(queryUrl, function(error, response, body){
-
-      if (!error) {
-          var body = JSON.parse(body);
-          console.log("Title: " + body.Title);
-          console.log("Year: " + body.Year);
-          console.log("IMDb Rating: " + body.imdbRating);
-          console.log('rottenRating: ' + movieObject.rottenRating);
-			    console.log('rottenURL: ' + movieObject.tomatoURL);
-          console.log("Country: " + body.Country);
-          console.log("Language: " + body.Language);
-          console.log("Plot: " + body.Plot);
-          console.log("Actors: " + body.Actors);
-
-          if (userChoice === undefined) {
-           userChoice = 'The Wold of Wall Street';
-           console.log("Watch this movie!");
-          }
-          
-
-      }
-
-  });
-  
-}
-
-
-
-
-//request("http://.omdbapi.com/?t=the+wolf+of+wall+street&y=&plot=full&apikey=50483e6b", function(error, response,body) {
   // if (!error && response.statusCode === 200) {
-    //   console.log("The movie's rating" + JSON.parse(body).imdbRating);
+    //   console.log('');
+      // console.log ('Title' + JSON.parse(body).Title);
+       //console.log('Year: '+ JSON.parse(body).year);
+       //console.log('IMDB Rating: '+ JSON.parse(body).imdRbating);
+       //console.log('Rotten Tomatoes Rating:' + JSON.parse(body).tomatoRating);
+       //console.log('Rotten Tomatoes URL: ' + JSON.parse(body).tomatoURL);
+       //console.log ('Country: ' + JSON.parse(body).Country);
+       //console.log('Language: ' + JSON.parse(body).Language);
+       //console.log('Plot: ' + JSON.parse(body).Plot);
+       //console.log('Actors:' + JSON.parse(body).Actors);
+       //console.log('');
+
+
+  // } else {
+    //console.log(err);
+   //} 
    //}
 //});
-
- 
-
-
-
